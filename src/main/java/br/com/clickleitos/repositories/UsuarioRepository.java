f211bd627bdac3565571f0fda1c809d7606afea9
Cicero Jonyedson <cicerojonyedison@gmail.com>
2020-06-01 13:36:41 -0300

    feat: configuration JWT and Security

--- src/main/java/br/com/clickleitos/repositories/UsuarioRepository.java
+++ src/main/java/br/com/clickleitos/repositories/UsuarioRepository.java
@@ -4,9 +4,12 @@ package br.com.clickleitos.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.clickleitos.domain.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Repository
 public interface  UsuarioRepository extends JpaRepository<Usuario, Long>{
-	
-	
+
+    @Transactional(readOnly = true)
+    Usuario findByEmail(String email);
 }
